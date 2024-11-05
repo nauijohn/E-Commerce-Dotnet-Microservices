9aea91777bbe1208d78f365d6c2851cfc44cb9ed
nauijohn <85679013+nauijohn@users.noreply.github.com>
2024-11-05 17:05:43 +0800

    FluentValidation Setup

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+// public record GetProductByCategoryRequest();
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (ISender sender, string category) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(category));
+
+            var response = result.Adapt<GetProductByCategoryResponse>();
+
+            return Results.Ok(response);
+        })
+            .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get products by category")
+            .WithDescription("Get all products in the catalog by category");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+
+// public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (ISender sender, Guid id) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+           .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get product by id")
+            .WithDescription("Get a product in the catalog by its id");
+    }
+}
