a7710d107beaed74f767185fda92e0fd5f912cc5
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-02-08 20:18:29 +0800

    Generated Proto Service Class from Discount proto File in Discount Grpc service

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -3,7 +3,7 @@
 public class Coupon
 {
     public int Id { get; set; }
-    public int ProductName { get; set; } = default!;
+    public string ProductName { get; set; } = default!;
     public string Description { get; set; } = default!;
     public int Amount { get; set; }
 }
